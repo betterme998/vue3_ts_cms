a4527ea153bc23f8ff9bae6de04bfa262a134cfb
betterme998 <betterme998@163.com>
2023-08-08 16:55:24 +0800

    项目初始化

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+// .vue文件类型声明
+declare module "*.vue" {
+  import { DefineComponent } from "vue"
+
+  const component:DefineComponent
+  export default component
+}
