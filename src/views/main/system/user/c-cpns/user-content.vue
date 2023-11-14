a37a27a9ffd5c3a4462f8b6086b01b8327c51151
betterme998 <betterme998@163.com>
2023-11-14 17:05:38 +0800

    请求用户列表

--- src/views/main/system/user/c-cpns/user-content.vue
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -12,11 +12,21 @@
       <el-button type="primary">新建用户</el-button>
     </div>
     <div class="table">
-      <ul>
-        <li v-for="item in usersList" :key="item.id">
-          {{ item.name }}
-        </li>
-      </ul>
+      <el-table :data="usersList" style="width: 100%">
+        <el-table-column align="center" type="selection" width="50px" />
+        <el-table-column align="center" type="index" label="序号" width="60px" />
+        <el-table-column align="center" prop="name" label="用户名" width="150px" />
+        <el-table-column align="center" prop="name" label="真实姓名" width="150px" />
+        <el-table-column align="center" prop="cellphone" label="手机号码" width="150px" />
+        <el-table-column align="center" prop="enable" label="状态" width="100px" />
+        <el-table-column align="center" prop="createAt" label="创建时间" />
+        <el-table-column align="center" prop="updateAt" label="更新时间" />
+
+        <el-table-column align="center" label="操作" width="160px">
+          <el-button size="small" icon="Edit" type="primary" text>编辑</el-button>
+          <el-button size="small" icon="Delete" type="danger" text>删除</el-button>
+        </el-table-column>
+      </el-table>
     </div>
     <div class="pagination">分页</div>
   </div>
@@ -47,6 +57,17 @@ const { usersList } = storeToRefs(systemStore)
   margin-bottom: 10px;
   .title {
     font-size: 22px;
+  }
+}
+
+.table {
+  :deep(.el-table__cell) {
+    padding: 12px 0;
+  }
+
+  :deep(.el-button) {
+    margin-left: 0;
+    padding: 5px 8px;
   }
 }
 </style>
